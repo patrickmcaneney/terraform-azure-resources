9b96a9b464d2d8cfe3438eb3f1dc4eb1b9931dbd
patrickmcaneney <68237714+patrickmcaneney@users.noreply.github.com>
2024-05-02 16:16:23 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+    backend "azurerm" {
+    resource_group_name  = "rg-terraform-state-01"
+    storage_account_name = "tfstate8399"
+    container_name       = "basic-lz-02"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+  
+}
+
+module "hub_and_spoke" {
+  source               = "azurerm/resources/azure//modules/pattern_hub_and_spoke"
+  location             = "eastus"
+  firewall             = false
+  gateway              = false
+  bastion              = false
+  address_space_hub    = ["10.100.0.0/24"]
+  address_space_spokes = [
+    {
+      workload      = "app1"
+      environment   = "dev"
+      instance      = "001"
+      address_space = ["10.100.10.0/24"]
+    },
+    {
+      workload      = "app1"
+      environment   = "prd"
+      instance      = "001"
+      address_space = ["10.100.11.0/24"]
+    }
+  ]
+}
