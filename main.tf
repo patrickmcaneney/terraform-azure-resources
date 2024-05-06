9455efb89a2a59675b83b63d9e77850071a3aafa
patrickmcaneney <68237714+patrickmcaneney@users.noreply.github.com>
2024-05-06 08:00:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,28 +19,35 @@ provider "azurerm" {
 }
 
 module "hub_and_spoke" {
-  source               = "azurerm/resources/azure//modules/pattern_hub_and_spoke"
-  location             = "eastus"
-  firewall             = false
-  gateway              = false
-  bastion              = false
-  address_space_hub    = ["10.100.0.0/24"]
+  source                                 = "azurerm/resources/azure//modules/pattern_hub_and_spoke"
+  location                               = "eastus"
+  firewall                               = true
+  gateway                                = false
+  bastion                                = false
+  address_space_hub                      = ["10.100.0.0/24"]
+  spoke_dns                              = true
+  address_space_spoke_dns                = ["10.100.1.0/24"]
+  spoke_dmz                              = true
+  address_space_spoke_dmz                = ["10.100.2.0/24"]
+  web_application_firewall               = false
+  private_monitoring                     = true
+  address_space_spoke_private_monitoring = ["10.100.3.0/27"]
+  connection_monitor                     = true
+  update_management                      = true
   address_space_spokes = [
     {
-      workload      = "app1"
-      environment   = "dev"
-      instance      = "001"
-      address_space = ["10.100.10.0/24"]
+      workload        = "shared"
+      environment     = "prd"
+      instance        = "001"
+      address_space   = ["10.100.5.0/24"]
       virtual_machine = false
-      linux_virtual_machine = 5
-      windows_virtual_machine = 0
     },
     {
-      workload      = "app1"
-      environment   = "prd"
-      instance      = "001"
-      address_space = ["10.100.11.0/24"]
-       virtual_machine = false
+      workload        = "app1"
+      environment     = "dev"
+      instance        = "001"
+      address_space   = ["10.100.10.0/24"]
+      virtual_machine = false
     }
   ]
 }
